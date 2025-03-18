ee93bbf5dd67d2ff5aee383ea84a52e8f4fde913
hammad-r-javed <128047144+hammad-r-javed@users.noreply.github.com>
2025-03-18 00:36:38 +0000

    fixing run script

--- run.sh
+++ run.sh
@@ -14,15 +14,20 @@ echo "exec i3" >> .xinitrc
 
 # dotfiles
 git clone https://github.com/hammad-r-javed/dotfiles.git
+mkdir .config
 
 # configure i3 
+mkdir .config/i3
 cp dotfiles/i3/config .config/i3/
 sudo cp dotfiles/i3/i3status.conf /etc/
+ls -la .config/i3
 
 # configure nvim
 mkdir .config/nvim
 cp -r dotfiles/nvim/lua .config/nvim
 cp dotfiles/nvim/init.lua .config/nvim
+ls -la .config/nvim
 
 # finish
+read -p "press Enter to run 'startx': "
 startx
